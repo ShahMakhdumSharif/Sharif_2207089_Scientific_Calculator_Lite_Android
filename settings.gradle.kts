9c31ada4ee5ad1c11c3a687ed36a52d7518e98ed
Shah Makhdum Sharif <shahmsharif2003@gmail.com>
2026-01-11 17:03:29 +0600

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sharif_2207089_Scientific_Calculator_Lite_Android"
+include(":app")
